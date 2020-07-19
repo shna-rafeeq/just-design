a483b969d61b9f5bcb79ddfb618e581ea94e3370
shna-rafeeq <shnar.majeed@gmail.com>
2020-07-19 15:58:14 +0300

    login modal

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,10 @@
-import React from "react";
-import { Navbar, Nav, Button } from "react-bootstrap";
+import React, { useState } from "react";
+import { Navbar, Nav, Button, Modal, Form, Row } from "react-bootstrap";
+
 
 export default function Navbar1() {
+  const [show, setShow] = useState(false);
+  let toogle = () => setShow((prev) => !prev);
   return (
     <>
       <style type="text/css">
@@ -18,6 +21,13 @@ height: "40px";
         background-color: #18A0FB;
         color: white;
     }
+    .custom-dialog{
+      margin-top: 12%;
+      max-width: none;
+      width:50%;
+  
+    }
+
     `}
       </style>
 
@@ -37,12 +47,46 @@ height: "40px";
             </Nav.Link>
           </Nav>
 
-          <Button variant="login" className="px-4 py-1">
-            Log In
+
+
+          <Button
+            show={show}
+            variant="login"
+            onClick={toogle}
+            style={{ marginTop: "10px" }}
+          >
+            Login
           </Button>
-          <Button variant="register" className="px-4 py-1">
-            Register
-          </Button>
+
+          <Modal closeButton show={show} onHide={toogle} dialogClassName="custom-dialog" >
+            <Modal.Header closeButton>  <h1>Log in Now</h1></Modal.Header>
+            <Modal.Body style={{ height: '300px' }}>
+              <div style={{ float: 'right' }}>
+                <img
+                  src={require("../image/login.png")}
+                  alt="my pic"
+                  // className="rounded w-75 mb-5 ml-5"
+                  style={{ width: '240px', height: '270px' }}
+                />
+
+              </div>
+              <Form >
+                <Form.Row >
+                  <Form.Group controlId="formGroupEmail">
+                    <Form.Control type="email" placeholder="Enter email" style={{ width: '494px', height: '40px', marginTop: '30px' }} />
+                  </Form.Group>
+                </Form.Row>
+                <Form.Row>
+                  <Form.Group controlId="formGroupPassword">
+                    <Form.Control type="password" placeholder="Password" style={{ width: '494px', height: '40px', marginTop: '15px' }} />
+                  </Form.Group>
+                </Form.Row>
+                <Form.Row>
+                  <Button variant='register' style={{ width: '130px', height: '40px', boxShadow: '3px 3px 10px rgba(0, 0, 0, 0.25)' }}> Register</Button>
+                </Form.Row>
+              </Form>
+            </Modal.Body>
+          </Modal>
         </Navbar.Collapse>
       </Navbar>
     </>
